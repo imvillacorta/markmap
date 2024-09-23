9b2ed0d1683deb0d1c3bbf809cc6697d0603657a
Ivan Villacorta <ivan_mv23@hotmail.com>
2024-09-23 08:33:20 -0300

    Implementação da lib markmap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgxMarkdownItComponent } from 'ngx-markdown-it';
+
+import { markmap as MarkmapView } from 'markmap-lib/dist/view';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export default class AppComponent {
+  @ViewChild('ngxMarkdownIt') ngxMarkdownIt?: NgxMarkdownItComponent;
   title = 'markmap';
+
+  markdown = `
+\`\`\`mindmap
+# root
+## child1
+  - child3
+## child2
+  - child3
+\`\`\`
+  `;
+
+  markdownReady(): void {
+    if (this.ngxMarkdownIt) {
+      this.ngxMarkdownIt.element.nativeElement
+        .querySelectorAll('.markmap-svg')
+        .forEach((mindmap) =>
+          MarkmapView(mindmap, JSON.parse(mindmap.innerHTML))
+        );
+    }
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import AppComponent from './app.component';
+
+import { NgxMarkdownItModule } from 'ngx-markdown-it';
+import { NgxMarkdownItConfig } from 'ngx-markdown-it';
+
+import { default as markmapPlugin } from 'markdown-it-markmap';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxMarkdownItModule.forRoot({
+      plugins: [markmapPlugin],
+    } as NgxMarkdownItConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
